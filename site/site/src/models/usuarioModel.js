2bdf47f8ea4737ed497a0aabc930b6821c6899f1
Eduardabrasilc <maria.costa@sptech.school>
2023-11-27 20:55:20 -0300

    login e cadastro funcionando

--- site/site/src/models/usuarioModel.js
+++ site/site/src/models/usuarioModel.js
@@ -1,9 +1,9 @@
-var database = require("../database/config")
+var database = require("../database/config");
 
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT idUsuario, nomeUsuario, emailUsuario, senhaUsuario FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nomeUsuario, emailUsuario, senhaUsuario FROM usuario WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
